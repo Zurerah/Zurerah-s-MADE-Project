11e5c01bf6bca9630d16b3c0f3fdd52e27b49cef
Shahid <zurerah.shahid@siemens-healthineers.com>
2024-06-13 11:21:26 +0200

    automated tests added

--- project/pipeline.py
+++ project/pipeline.py
@@ -9,11 +9,11 @@ import pandas as pd
 
 # %%
 df = pd.read_csv('https://data.humdata.org/dataset/469a7757-302c-4176-bf82-4a4523feb586/resource/5419549c-8d27-497c-8a7a-9d849a86cbca/download/climate-change_deu.csv')
-df
+
 
 # %%
 df = pd.read_csv('https://data.humdata.org/dataset/469a7757-302c-4176-bf82-4a4523feb586/resource/5419549c-8d27-497c-8a7a-9d849a86cbca/download/climate-change_deu.csv')
-df
+
 
 # %%
 df.dropna()
